264b83838ef495c48dd7af7713b7beb6b780fcf1
JoacoGar <joaquins.garcia@correo.ucu.edu.uy>
2026-03-17 01:38:05 -0300

    se terminan 3, 4 y 5

--- src/main/java/com/Equipo4App/UtilMath.java
+++ src/main/java/com/Equipo4App/UtilMath.java
@@ -1,15 +1,25 @@
 package com.Equipo4App;
 
 public class UtilMath{
+
+    /*
+     Casos borde:
+     - 0! = 1
+     - 1! = 1
+     - negativos: no definido → se retorna -1
+     - valores grandes pueden desbordar int
+    */
     public static int factorial (int num){
-        if (num == 0) return 1;
-        if (num == 1) return 1;
+        if (num < 0) return -1;
+        if (num == 0 || num == 1) return 1;
+
         int resultado = 1;
-        for (int i = 1; i <= num; i++){
-            resultado = resultado * factorial(num - 1);
+        for (int i = 2; i <= num; i++){
+            resultado = resultado * i;
         }
-        return 0;
+        return resultado;
     }
+
     public static boolean esPrimo(int num){
         if (num<2) return false;
         if (num==2) return true;
@@ -17,6 +27,40 @@ public class UtilMath{
         for (int i=3;i*i<=num;i+=2)
             if (num%i==0) return false;
         return true;
+    }
 
+    // suma de pares con WHILE
+    public static int sumaPares(int num){
+        int suma = 0;
+        int i = 0;
+        while(i <= num){
+            if(i % 2 == 0){
+                suma += i;
+            }
+            i++;
+        }
+        return suma;
+    }
+
+    // suma de impares con WHILE
+    public static int sumaImpares(int num){
+        int suma = 0;
+        int i = 0;
+        while(i <= num){
+            if(i % 2 != 0){
+                suma += i;
+            }
+            i++;
+        }
+        return suma;
+    }
+
+    // metodo reutilizable que decide
+    public static int sumaSegunPrimo(int num){
+        if(esPrimo(num)){
+            return sumaPares(num);
+        }else{
+            return sumaImpares(num);
+        }
     }
 }
